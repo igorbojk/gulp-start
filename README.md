7b88ef59ee430bdfca9d73538c873f2c49d61537
igorbjk <igorbojk@gmail.com>
2018-01-22 00:20:40 +0200

    stage save

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # gulp-start
 Start template for adaptive site layout.
-Include: Html5 template, gulp, scss preprocessor and jade.
+Include: Html5 template, gulp, scss, jade and minify CSS with CSSO.
+
+# Instruction
+1) npm i
+2) gulp build
+3) gulp
+
+
+# Attention. Comments are only available in a non-minified file
--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,12 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     batch = require('gulp-batch'),
     autoprefixer = require('gulp-autoprefixer'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    concat = require('gulp-concat'),
+    csso = require('gulp-csso');
 
 gulp.task('gulpJade',function(){
-    return gulp.src('src/app/**/*.jade')
+    return gulp.src('src/app/template/**/*.jade')
     .pipe(gulpJade({
             jade: jade,
             pretty: true
@@ -17,7 +19,7 @@ gulp.task('gulpJade',function(){
             console.warn(err.stack, '\n');
             console.log('\n');
         }))
-    .pipe(gulp.dest('src/builds/teplates'))
+    .pipe(gulp.dest('src/build'))
 });
 
 gulp.task('sass', function(){
@@ -27,20 +29,54 @@ gulp.task('sass', function(){
             browsers: ['last 50 versions'],
             // cascade: false
         }))
-    .pipe(gulp.dest('src/build/style'))
+    .pipe(gulp.dest('src/app/libs/style'))
 });
 
-gulp.task('watch:html', function(){
+gulp.task('style', function(){
+    return gulp.src('src/app/libs/style/**/*.css')
+        .pipe(concat('libs.css')
+        .on('error', function(err){
+            console.log('\n');
+            console.log(err.name + ': ' + err.message);
+            console.warn(err.stack, '\n');
+            console.log('\n');
+        }))
+        .pipe(csso())
+        .pipe(gulp.dest('src/build/css'))
+});
 
-    watch('src/**/*.jade', batch(function(events, done){
+gulp.task('scripts', function(){
+    return gulp.src('src/app/js/**/*.js') 
+        .pipe(concat('libs.js')
+        .on('error', function(err){
+            console.log('\n');
+            console.log(err.name + ': ' + err.message);
+            console.warn(err.stack, '\n');
+            console.log('\n');
+        }))
+        .pipe(gulp.dest('src/build/js'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('watch', function(){
+    
+    gulp.watch('src/app/template/**/*.jade', batch(function(events, done){
         gulp.start('gulpJade',done);
     }));
 
-    watch('src/**/*.+(scss|sass)', batch(function(events, done) {
+    gulp.watch('src/app/js/**/*.js', batch(function(events, done){
+        gulp.start('scripts',done);
+    }));
+
+    gulp.watch('src/app/style/**/*.+(scss|sass)', batch(function(events, done) {
         gulp.start('sass', done);
+    }));
+
+    gulp.watch('src/app/libs/style/**/*.css', batch(function(events, done) {
+        gulp.start('style', done);
     }));
 });
 
-gulp.task('build', ['sass', 'gulpJade']);
+gulp.task('build', ['sass',  'gulpJade', 'style']);
 
-gulp.task('default', ['watch:html']);
+gulp.task('default', ['watch']);
